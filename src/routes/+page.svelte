30e651c7a9f67b7e4494a833d8a82515443bc634
Alejandro Sáez <alejandro.sapa@gmail.com>
2025-02-22 14:33:23 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>Vivi Palominos - Agente de viajes</title>
+</svelte:head>
+
+<main class="container mx-auto h-dvh flex items-center justify-center">
+	<div class="w-fit text-center">
+        <img src="/logo.svg" alt="Logo de vivipalominos.com" class="w-60 mx-auto">
+	</div>
+</main>
