0650a56d03dad7c03bc1e1eaac5b812ff8b642ae
Alejandro Sáez <alejandro.sapa@gmail.com>
2025-03-24 21:16:41 -0300

    working on hero section

--- README.md
+++ README.md
@@ -1,38 +1,32 @@
-# sv
+# Vivipalominos.com
 
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
+## Description
+Landing page de la agente de viajes Viviana Palominos.
 
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+## Installation
+Instructions on how to install and set up the project.
 
 ```bash
-npm run dev
+# Clone the repository
+git clone https://github.com/yourusername/yourproject.git
 
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
+# Navigate to the project directory
+cd yourproject
+
+# Install dependencies
+pnpm install
 ```
 
-## Building
-
-To create a production version of your app:
+## Usage
+Instructions on how to use the project.
 
 ```bash
-npm run build
+# Run the project
+npm start
 ```
 
-You can preview the production build with `npm run preview`.
+## Contributing
+Guidelines for contributing to the project.
 
-> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
+## License
+Information about the project's license.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,31 @@
 <svelte:head>
-    <title>Vivi Palominos - Agente de viajes</title>
+	<title>Vivi Palominos - Agente de viajes</title>
 </svelte:head>
 
-<main class="container mx-auto h-dvh flex items-center justify-center">
-	<div class="w-fit text-center">
-        <img src="/logo.svg" alt="Logo de vivipalominos.com" class="w-60 mx-auto">
-	</div>
+<main class="bg-cloud relative mx-auto flex h-dvh items-center justify-center">
+	<nav class="absolute top-0 z-10 w-full bg-white/20 p-4 backdrop-blur-sm">
+		<div class="container mx-auto flex items-center justify-between">
+			<img src="/logo.svg" alt="Logo de vivipalominos.com" class="w-60" />
+			<button>Whatsapp</button>
+		</div>
+	</nav>
+	<header class="absolute top-0 z-0 w-full">
+		<picture>
+			<source srcset="/hero-big.webp" type="image/webp" />
+			<img src="/hero-big.jpg" alt="Hero" class="h-[500px] w-full object-cover" />
+		</picture>
+        <!-- AIRPLAIN -->
+        <div>
+            <img src="/airplain.png" class="absolute z-20 top-20 w-80 right-0 md:right-36 md:top-10 md:w-[600px]" alt="" aria-hidden="true">
+        </div>
+        <!-- SEPARATOR DESKTOP -->
+		<div class="absolute -bottom-36 hidden md:block w-full rotate-6 right-0">
+			<img src="/separator.svg" class="w-full object-contain" alt="" aria-hidden="true" />
+		</div>
+        <!-- SEPARATOR MOBILE -->
+        <div class="absolute -bottom-72 md:hidden w-full">
+            <!-- This image is decorative and serves as a visual separator -->
+            <img src="/separator-sm.svg" class="w-full" alt="" role="separator" aria-hidden="true" />
+		</div>
+	</header>
 </main>
